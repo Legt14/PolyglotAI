b3cd134b35a4fc809c83af7bcca3ce7afb5f94ae
root <root@DESKTOP-DFSBO27>
2023-04-19 08:18:08 -0400

    fix security of api key

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    OPENAI_API_KEY: "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",
-  },
 };
 
 module.exports = nextConfig;
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,7 +4,7 @@ import { Configuration, OpenAIApi } from "openai";
 export async function connection(text: string, lang: string) {
   const configuration = new Configuration({
     organization: "org-89XGHoeg68x692lZaqWwJ5k7",
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
   });
   const openai = new OpenAIApi(configuration);
   const response = await openai.createChatCompletion({
