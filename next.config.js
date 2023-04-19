fd44b2a2e16c14fda78cff4bfa29c40882106f6c
root <root@DESKTOP-DFSBO27>
2023-04-19 09:43:51 -0400

    changes in next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'out',
 };
 
 module.exports = nextConfig;
--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 interface Social {
   name: string;
   img: string;
