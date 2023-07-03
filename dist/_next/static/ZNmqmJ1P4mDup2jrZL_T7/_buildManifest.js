a4e66ea810a4c1b19ac1a4b944e023945d50c724
Leandro <legt1407@gmail.com>
2023-07-03 19:39:39 -0400

    update

---
+++ dist/_next/static/ZNmqmJ1P4mDup2jrZL_T7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/485-751f86feca33e703.js","static/chunks/pages/index-64e8dce856ff5c69.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
